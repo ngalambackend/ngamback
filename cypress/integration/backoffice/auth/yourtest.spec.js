0e6f379aa65330b5c2c8ce9293357e8373179bc7
Oim <trustoim@gmail.com>
2020-07-28 21:26:57 +0700

    test 2

--- cypress/integration/backoffice/auth/yourtest.spec.js
+++ cypress/integration/backoffice/auth/yourtest.spec.js
@@ -3,5 +3,9 @@ describe('I want test your hearth', function() {
     it('Access url link', () => {
         cy.url('http://ngalambackend.test')
     });
+
+    it('Nothing', () => {
+        
+    });
 });
     
